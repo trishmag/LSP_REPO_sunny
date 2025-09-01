e46412da7629de5e48d03dbcf099fef4cf7e194c
trishmag <misslittletrish@gmail.com>
2025-09-01 19:20:36 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * Name: Student Developer
+ */
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * HelloWorld class that demonstrates a simple Java program
+ * for LSP Assignment 1.
+ * 
+ * This class contains a main method that prints "Hello World!!"
+ * to the console.
+ * 
+ * @author Student Developer
+ * @version 1.0
+ */
+public class HelloWorld {
+    
+    /**
+     * Main method that serves as the entry point for the program.
+     * Prints "Hello World!!" to the console.
+     * 
+     * @param args command line arguments (not used in this program)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
